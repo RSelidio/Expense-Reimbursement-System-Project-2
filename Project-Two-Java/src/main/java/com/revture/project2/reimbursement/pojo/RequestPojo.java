3072317abb0765b731d50b2b42fb85df983eecb9
Rowel Selidio <59985283+RSelidio@users.noreply.github.com>
2022-01-04 00:23:29 -0700

    Update RequestPojo.java

--- Project-Two-Java/src/main/java/com/revture/project2/reimbursement/pojo/RequestPojo.java
+++ Project-Two-Java/src/main/java/com/revture/project2/reimbursement/pojo/RequestPojo.java
@@ -52,4 +52,106 @@ public class RequestPojo {
 	}
 
 
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getReqName() {
+		return reqName;
+	}
+
+
+	public void setReqName(String reqName) {
+		this.reqName = reqName;
+	}
+
+
+	public int getReqPrice() {
+		return reqPrice;
+	}
+
+
+	public void setReqPrice(int reqPrice) {
+		this.reqPrice = reqPrice;
+	}
+
+
+	public String getReqDescription() {
+		return reqDescription;
+	}
+
+
+	public void setReqDescription(String reqDescription) {
+		this.reqDescription = reqDescription;
+	}
+
+
+	public boolean isReqFlag() {
+		return reqFlag;
+	}
+
+
+	public void setReqFlag(boolean reqFlag) {
+		this.reqFlag = reqFlag;
+	}
+
+
+	public Date getReqDate() {
+		return reqDate;
+	}
+
+
+	public void setReqDate(Date reqDate) {
+		this.reqDate = reqDate;
+	}
+
+
+	public LocalDate getAcceptDate() {
+		return acceptDate;
+	}
+
+
+	public void setAcceptDate(LocalDate acceptDate) {
+		this.acceptDate = acceptDate;
+	}
+
+
+	public String getImg() {
+		return img;
+	}
+
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+
+	public int getUsersId() {
+		return usersId;
+	}
+
+
+	public void setUsersId(int usersId) {
+		this.usersId = usersId;
+	}
+
+
+	public boolean isReject() {
+		return reject;
+	}
+
+
+	public void setReject(boolean reject) {
+		this.reject = reject;
+	}
+	
+	
+
+
 }
